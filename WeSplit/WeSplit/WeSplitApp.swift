d76e51ee96eb68bb3cd5f488594d8283a1b064d7
Alex <86182397+alehxs@users.noreply.github.com>
2024-06-17 00:04:32 -0500

    Add day 18: Project 1, Pt. 3

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alex on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
