b806722b77a133fcecd715ac4de3953fe0a74e93
Alex <86182397+alehxs@users.noreply.github.com>
2024-06-25 18:08:08 -0500

    Day 25: Challenge init

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Alex on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
