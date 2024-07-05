2bb4dc714db3c3b31136722510ab3c9f81048b89
Alex <86182397+alehxs@users.noreply.github.com>
2024-07-05 05:20:09 -0500

    Day 26: Project 1, pt. 1

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var SleepAmount = 6.5
+    
     
     var body: some View {
-        Stepper("\(SleepAmount.formatted()) hours", value: $SleepAmount, in: 4...12, step: 0.45)
+        Text("hey")
     }
+
+    
 }
 
 #Preview {
     ContentView()
 }
+
+
+///Basic introduction to stepper picker
+//@State private var SleepAmount = 6.5
+//
+//var body: some View {
+//    Stepper("\(SleepAmount.formatted()) hours",  $SleepAmount, in: 4...12, step: 0.45) // Stepper uses + or - to inc or dec value
+//}
+
+///Introduction to date pickers and some properites
+
+//struct ContentView: View {
+//    @State private var wakeUp = Date.now
+//    
+//    var body: some View {
+//        DatePicker("Please enter a date", selection: $wakeUp, in: ...Date.now)
+//            .padding()
+//            .labelsHidden()
+//    }
+//    func exampleDates() {
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//        let range = Date.now...tomorrow
+//    }
+//}
+
+///Date formatting and how to make working with dates easier
+
+//struct ContentView: View {
+//    
+//    
+//    var body: some View {
+////        Text(Date.now, format: .dateTime.day().month().year())
+//        Text(Date.now.formatted(date: .abbreviated, time: .shortened))
+//    }
+//    
+//    func exampleDates(){
+////        var components = DateComponents()
+////        components.hour = 8
+////        components.minute = 0
+////        let date = Calendar.current.date(from: components) ?? .now
+//        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+//        let hour = components.hour ?? 0
+//        let minute = components.minute ?? 0
+//    }
+//    
+//}
