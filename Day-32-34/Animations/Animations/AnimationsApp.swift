8fd58e25f7760eb374c22c1509b6353b6d7d8c35
Alex <86182397+alehxs@users.noreply.github.com>
2024-07-18 22:25:43 -0500

    Initalize Animations

---
+++ Day-32-34/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Alex on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day-32-34/Animations/Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Alex on 7/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
